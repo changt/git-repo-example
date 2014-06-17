0dac01285f2254a3bb69a739332b077b0bfdf2f6
Robert Soto <robsoto@mit.edu>
2014-06-17 14:33:37 -0400

    greeting added to Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,10 @@ public class Hello {
     public static void main(String[] args) {
         printGreeting();
     }
+
+
+
+//hello from robsoto 
     
     public static void printGreeting() {
         System.out.println("Hello, version control!");
