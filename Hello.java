1869a205e0a2a81219c71da91e2c83ed7096de50
Sen Chang <tonych@mit.edu>
2014-06-17 14:33:17 -0400

    add hello to java file

--- Hello.java
+++ Hello.java
@@ -3,8 +3,9 @@ public class Hello {
     public static void main(String[] args) {
         printGreeting();
     }
-    
+        //hello from changt
     public static void printGreeting() {
         System.out.println("Hello, version control!");
     }
+
 }
